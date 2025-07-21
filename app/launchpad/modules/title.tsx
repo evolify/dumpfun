407d85ce5c9c223794a4c6e65193cf91abdbfd87
evolify <wolf@evolify.cn>
2025-07-21 15:52:55 +0800

    fix: add suspense

--- app/launchpad/modules/title.tsx
+++ app/launchpad/modules/title.tsx
@@ -12,7 +12,7 @@ interface Props {
   data: Promise<LaunchpadsInfo[]>
 }
 
-export default function Title(props: Props) {
+function TitleContent(props: Props) {
   const data = use(props.data)
   const launchpad = useQuery("launchpad") as Launchpad
   const launchpadConfig = LaunchpadConfig[launchpad]
@@ -22,14 +22,20 @@ export default function Title(props: Props) {
   }, [data])
 
   return (
-    <Suspense>
-      <div className="flex flex-row items-center gap-2 flex-1">
-        <Icon url={getFavicon(launchpadConfig?.home)} size={16} />
-        <div className="text-xl font-bold">{launchpadConfig?.name}</div>
-        <div className="text-sm text-gray-400 ml-4">
-          {formatNumber(launchpadInfo?.liquidity)}
-        </div>
+    <div className="flex flex-row items-center gap-2 flex-1">
+      <Icon url={getFavicon(launchpadConfig?.home)} size={16} />
+      <div className="text-xl font-bold">{launchpadConfig?.name}</div>
+      <div className="text-sm text-gray-400 ml-4">
+        {formatNumber(launchpadInfo?.liquidity)}
       </div>
+    </div>
+  )
+}
+
+export default function Title(props: Props) {
+  return (
+    <Suspense>
+      <TitleContent {...props} />
     </Suspense>
   )
 }
