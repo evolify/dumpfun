94ae1c7a79c2d3d31bb7c1fbb7b57795f48d3796
evolify <wolf@evolify.cn>
2025-08-04 16:17:08 +0800

    fix: fix type error

--- app/utils.ts
+++
@@ -1,5 +0,0 @@
-import type { Duration, LaunchpadsInfo, LaunchpadsStats } from "@/types"
-
-export function getStats(data: LaunchpadsInfo, duration: Duration) {
-  return data[`stats${duration}`] as LaunchpadsStats
-}
